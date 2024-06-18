c3880bad90ef148da133c91398bd5d0a4f109a14
arielGs_Dev <arielogavilanes1534@gmail.com>
2024-06-17 22:33:34 -0500

    merging

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,5 +1,6 @@
 import 'package:app_flutter_on_code_mobile/components/custom_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
@@ -9,27 +10,74 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
+  final List<String> images = [
+    'assets/images/python-curso.jpg',
+    'assets/images/nest.jpeg',
+    'assets/images/javaScript.jpg',
+    'assets/images/c++.jpg',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // toolbarHeight: 75,
-        backgroundColor: const Color.fromRGBO(255, 255, 255, 0.1),
-        title: const Text(
-          'Dashboard',
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-        actions: const [
+        title: const Text('Carousel'),
+      ),
+      body: Column(
+        children: [
           Align(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              child: Image(image: AssetImage('assets/images/onCodeMobile.png')),
+            alignment: Alignment.topCenter,
+            child: CarouselSlider(
+              options: CarouselOptions(
+                height: 200.0,
+                aspectRatio: 16 / 9,
+                viewportFraction: 0.8,
+                initialPage: 0,
+                enableInfiniteScroll: true,
+                reverse: false,
+                autoPlay: true,
+                autoPlayInterval: const Duration(seconds: 3),
+                autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enlargeCenterPage: true,
+                scrollDirection: Axis.horizontal,
+              ),
+              items: images.map((item) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: const BoxDecoration(
+                        color: Colors.amber,
+                      ),
+                      child: Image.network(item, fit: BoxFit.cover),
+                    );
+                  },
+                );
+              }).toList(),
             ),
-          )
+          ),
+          // Text below Carousel
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'Additional Information',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              'This is where you can add more details or any other content you want to display below the carousel.',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16.0),
+            ),
+          ),
         ],
       ),
-      drawer: const CustomDrawer(),
     );
   }
 }
